f0af846b198d30c57469228c3530d2436890c4e0
miheret-eng <84643392+miheret-eng@users.noreply.github.com>
2021-06-24 12:35:07 -0700

    0-14

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+
+
+/**
+
+ * main - check the code for ALX School students.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+  
+{
+more_numbers();
+return (0);
+}
