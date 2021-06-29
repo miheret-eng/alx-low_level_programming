365735f9c3737b9ef19f5d10d90ac6dafaab7f33
miheret-eng <84643392+miheret-eng@users.noreply.github.com>
2021-06-29 05:28:13 -0700

    takes a pointer to an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - set value of int to 98
+ * @n: address of value to change
+ * Description: resets *n to 98
+ * Return: nothing
+ **/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
